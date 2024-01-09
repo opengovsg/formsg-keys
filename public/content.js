e6e924257ce7b3d23334828c5d0a36c83a18816d
Ken <ken@open.gov.sg>
2024-01-09 21:45:31 +0800

    feat: key input

--- public/content.js
+++ public/content.js
@@ -1,8 +1,20 @@
-import { GET_KEY } from "../src/constants";
+import { GET_KEY, INSERT_KEY } from "../src/constants";
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.command === GET_KEY) {
     const keyData = document.querySelector("code.chakra-code");
     sendResponse(keyData.innerText);
+  }
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.command === INSERT_KEY) {
+    const keyInputEl = document.querySelector("[name='secretKey']");
+    keyInputEl.value = request.key;
+
+    ["input", "change", "keydown"].forEach((eventType) => {
+      const event = new Event(eventType, { bubbles: true });
+      keyInputEl.dispatchEvent(event);
+    });
   }
 });
--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import { getKeyFromStorage } from "./keys.utils";
-import { PAGE_NAV, SET_ID } from "./constants";
+import { PAGE_NAV, SET_ID, INSERT_KEY } from "./constants";
 import { getFormIdFromAdminUrl } from "./url.utils";
 
+const localstore = {};
 function setKeyToContent(formId, key) {
+  localstore[formId] = key;
+  localstore["currentId"] = formId;
   document.getElementById("content").innerHTML = `${formId}: ${key}`;
 }
 
@@ -24,6 +27,36 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
   if (!formId) {
     return;
   }
+
   const key = await getKeyFromStorage(formId);
   setKeyToContent(formId, key);
 });
+
+chrome.tabs.query({ active: true, currentWindow: true }, async function (tab) {
+  const url = tab[0].url;
+  const formId = getFormIdFromAdminUrl(url);
+
+  if (!formId) {
+    return;
+  }
+  const key = await getKeyFromStorage(formId);
+  setKeyToContent(formId, key);
+  handleInsertKey();
+});
+
+document
+  .getElementById("insertkey-btn")
+  .addEventListener("click", handleInsertKey);
+
+function handleInsertKey() {
+  const { currentId } = localstore;
+  if (currentId) {
+    const key = localstore[currentId];
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tab) {
+      chrome.tabs.sendMessage(tab[0].id, {
+        command: INSERT_KEY,
+        key,
+      });
+    });
+  }
+}
