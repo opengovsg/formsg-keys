25d4a4681f084d9ae1267ef1bc695def5c990d3c
Ken <ken@open.gov.sg>
2024-01-09 12:44:44 +0800

    fix: handle non admin url gracefuly

--- src/url.utils.js
+++ src/url.utils.js
@@ -2,5 +2,9 @@ const formIdRegex = new RegExp(
   /form\.gov\.sg\/admin\/form\/(?<formid>[0-9a-fA-F]{24})\/*.*/
 );
 export const getFormIdFromAdminUrl = (url) => {
-  return formIdRegex.exec(url)[1];
+  const result = formIdRegex.exec(url);
+  if (!result) {
+    return null;
+  }
+  return result[1];
 };
