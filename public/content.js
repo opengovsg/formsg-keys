a6dce472fc50f6d67d932dba4ded21bf4ffae6f3
Ken <ken@open.gov.sg>
2024-01-09 13:01:16 +0800

    feat: content.js to share common config

--- public/content.js
+++ public/content.js
@@ -1,5 +1,7 @@
+import { GET_KEY } from "../src/constants";
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.command === "GET_KEY") {
+  if (request.command === GET_KEY) {
     const keyData = document.querySelector("code.chakra-code");
     sendResponse(keyData.innerText);
   }
