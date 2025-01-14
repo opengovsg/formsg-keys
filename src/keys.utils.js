9dd71415e70f5702894f9aa169a5ab97c0a5f807
g-tejas <tejas@open.gov.sg>
2025-01-14 17:14:00 +0800

    feat: abstract download fn

--- src/keys.utils.js
+++ src/keys.utils.js
@@ -4,3 +4,11 @@ export const getKeyFromStorage = async (formId) => {
     return keyPairResult[formId];
   });
 };
+
+
+export const downloadKeyToStorage = async (formId, key) => {
+  const keyPair = { [formId]: key };
+  chrome.storage.local.set(keyPair, () => {
+    console.log(`Key (${key}) was saved in storage for formId (${formId})`);
+  });
+};
