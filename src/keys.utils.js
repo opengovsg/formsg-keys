d149bbca8b5ebc8178521ac6d01dbc0b7067358c
Tejas G. <code@gtejas.com>
2025-01-16 11:35:46 +0800

    Add add keys functionality (#1)
    * feat: abstract download fn
    
    * feat: add simple button to add keys
    
    * feat: make it pretty
    
    * feat: hide button if not on admin page
    
    * fix: rename fn and add margin

--- src/keys.utils.js
+++ src/keys.utils.js
@@ -4,3 +4,11 @@ export const getKeyFromStorage = async (formId) => {
     return keyPairResult[formId];
   });
 };
+
+
+export const setKeyToStorage = async (formId, key) => {
+  const keyPair = { [formId]: key };
+  chrome.storage.local.set(keyPair, () => {
+    console.log(`Key (${key}) was saved in storage for formId (${formId})`);
+  });
+};
